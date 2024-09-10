4c12cd63c1b7da8343b36f0e3d272dcd87db350f
Lovely Mandal <lmandal@adobe.com>
2024-09-10 14:03:01 +0530

    dev change 3

--- blocks/form/integrations/recaptcha.js
+++ blocks/form/integrations/recaptcha.js
@@ -26,7 +26,7 @@ export default class GoogleReCaptcha {
 
   loadCaptcha(form) {
     if (form && this.siteKey) {
-     // const submit = form.querySelector('button[type="submit"]');
+      const submit = form.querySelector('button[type="submit"]');
       const obs = new IntersectionObserver((entries) => {
         entries.forEach((entry) => {
           if (entry.isIntersecting) {
@@ -35,7 +35,7 @@ export default class GoogleReCaptcha {
           }
         });
       });
-      //obs.observe(submit);
+      obs.observe(submit);
     }
   }
 
