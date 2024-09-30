63e4a1eba867d26e18c581512132bbe48987c91a
Lovely Mandal <lmandal@adobe.com>
2024-09-30 21:54:32 +0530

    test

--- blocks/form/integrations/recaptcha.js
+++ blocks/form/integrations/recaptcha.js
@@ -10,7 +10,7 @@ export default class GoogleReCaptcha {
     this.id = id;
   }
 
-  #loadScript(url) {
+  #loadScriptV2(url) {
     if (!this.loadPromise) {
       this.loadPromise = new Promise((resolve, reject) => {
         const captchaWrapper = document.getElementsByClassName('captcha-wrapper')[0];
@@ -29,6 +29,20 @@ export default class GoogleReCaptcha {
     }
   }
 
+  #loadScript(url) {
+    if (!this.loadPromise) {
+      this.loadPromise = new Promise((resolve, reject) => {
+        const head = document.head || document.querySelector('head');
+        const script = document.createElement('script');
+        script.src = url;
+        script.async = true;
+        script.onload = () => resolve(window.grecaptcha);
+        script.onerror = () => reject(new Error(`Failed to load script ${url}`));
+        head.append(script);
+      });
+    }
+  }
+
   loadCaptcha(form) {
     if (form && this.config.siteKey) {
       const submit = form.querySelector('button[type="submit"]');
@@ -37,13 +51,15 @@ export default class GoogleReCaptcha {
           if (entry.isIntersecting) {
             const siteKey = this.config.siteKey;
             const url = this.config.uri ;
-            if(this.config.version == 'v2' || this.config.version == 'enterprise'){
-                this.#loadScript(url);
+            if(this.config.version == 'v2'){
+                this.#loadScriptV2(url);
             }
-            // else if(this.config.version == 'enterprise'){
-            //   let queryParams =  "?onload=onloadRecaptchaCallback&render=explicit";
-            //   this.#loadScript(url + queryParams);
-            // }
+            else if(this.config.version == 'enterprise'){
+              //let queryParams =  "?onload=onloadRecaptchaCallback&render=explicit";
+              //this.#loadScript(url + queryParams);
+              if(window.currentMode !=='edit')
+              this.#loadScript(url+"?render=" + siteKey);
+            }
             else{
               this.#loadScript('https://www.recaptcha.net/recaptcha/api.js?render=' + siteKey);
             }
